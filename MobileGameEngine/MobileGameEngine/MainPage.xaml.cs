88894fc31f109647e18388a7ab21e75225ff7b66
lunarum <remco@lunarum.nl>
2018-12-09 17:38:05 +0100

    first commit

---
+++ MobileGameEngine/MobileGameEngine/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MobileGameEngine
+{
+    public partial class MainPage : ContentPage
+    {
+        bool showFill = true;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            SKCanvasView canvasView = new SKCanvasView();
+            canvasView.PaintSurface += OnCanvasViewPaintSurface;
+            Content = canvasView;
+        }
+
+        private void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            SKPaint paint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = Color.Red.ToSKColor(),
+                StrokeWidth = 25
+            };
+
+            canvas.DrawCircle(info.Width / 2, info.Height / 2, 100, paint);
+
+            if (showFill)
+            {
+                paint.Style = SKPaintStyle.Fill;
+                paint.Color = SKColors.Blue;
+                canvas.DrawCircle(args.Info.Width / 2, args.Info.Height / 2, 100, paint);
+            }
+        }
+
+        void OnCanvasViewTapped(object sender, EventArgs args)
+        {
+            showFill ^= true;
+            (sender as SKCanvasView).InvalidateSurface();
+        }
+    }
+}
